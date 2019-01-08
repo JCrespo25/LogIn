04c100ed91941225422cdde73bab39c19e008586
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2019-01-08 11:48:51 -0800

    Eliminamos todo

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/notes-db-app', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
-.then(db = console.log('DB is connected'))
-.catch(err => console.error(err));
