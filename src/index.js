54187f6decef4d9ebed65d349b463ca98e625a86
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2019-01-09 15:20:33 -0800

    CRUD TERMINADO 3/7

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const flash = require('connect-flash');
+
 //Initializations
 const app = express();
 require('./database');
+
 //Settings
-app.set('port',process.env.PORT || 3000);
-app.set('views',path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
@@ -18,25 +21,31 @@ app.engine('.hbs', exphbs({
 app.set('view engine', 'hbs');
 
 //MiddleWares
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(session({
     secret: 'mysecretapp',
     resave: true,
     saveUninitialized: true
 }));
+app.use(flash());
+
 //Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 //Routes
 app.use(require('./routes/index'));
 app.use(require('./routes/notes'));
 app.use(require('./routes/users'));
+
 //Static Files
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Server is listening
-
-app.listen(app.get('port'), () =>
-{
+app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
 });
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -6,41 +6,59 @@ router.get('/notes/add', (req, res) =>
     res.render('notes/new-note')
 );
 
-router.post('/notes/new-note', async (req, res) => {
+router.post('/notes/new-note', async(req, res) => {
 
     const { title, description } = req.body;
     const errors = [];
 
-    if(!title)
-    {
-        errors.push({text: 'Por favor ingrese un titulo'});
+    if (!title) {
+        errors.push({ text: 'Por favor ingrese un titulo' });
     }
-    if(!description)
-    {
-        errors.push({text: 'Por favor ingrese una descripcion'});
+    if (!description) {
+        errors.push({ text: 'Por favor ingrese una descripcion' });
     }
-    if(errors.length > 0)
-    {
-        res.render('notes/new-note', 
-            {
-                errors,
-                title,
-                description
-            }
-        );
-    }
-    else
-    {
-        const newNote = new Note({title, description});
+    if (errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    } else {
+
+        //CREATE
+        const newNote = new Note({ title, description });
         await newNote.save();
-        res-redirect('/notes');
+        req.flash('success_msg', 'Nota creada satisfactoriamente');
+        res.redirect('/notes');
     }
 });
 
-router.get('/notes', async (req, res) => {
-    const notes = await Note.find();
+//LEER
+router.get('/notes', async(req, res) => {
+    const notes = await Note.find().sort({ date: 'desc' });
     res.render('notes/all-notes', { notes });
 });
 
+
+//LEER
+router.get('/notes/edit/:id', async(req, res) => {
+    const note = await Note.findById(req.params.id);
+    res.render('notes/edit-note', { note });
+});
+
+//ACTUALIZAR
+router.put('/notes/edit-note/:id', async(req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, description });
+    req.flash('success_msg', 'Nota actualizada satisfactoriamente');
+    res.redirect('/notes');
+});
+
+//DELETED
+router.delete('/notes/delete/:id', async(req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'Nota eliminada satisfactoriamente');
+    res.redirect('/notes');
+});
 
 module.exports = router;
