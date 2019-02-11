f093392f9ac2e1d11d15801769296a33d60699e0
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2019-02-11 14:35:13 -0800

    PENDIENTE sesion master, cliente

--- src/models/profiles/externos/clientes.js
+++ src/models/profiles/externos/clientes.js
@@ -3,26 +3,77 @@ const { Schema } = mongoose;
 const bcrypt = require('bcryptjs');
 
 
-const ProfileClientSchema = new Schema({
-    empresaName: { type: String, required: true },
-    empresaTradeName: { type: String, required: true },
+const clientes = new Schema({
+    empresaRazonSocial: { type: String, required: true },
+    empresaNombreComercial: { type: String, required: true },
     representanteName: { type: String, required: true },
-    representanteApPaterno: { type: String, required: true },
-    representanteApMaterno: { type: String },
-    contactoNumberFijo: { type: Number, required: true },
-    contactoCelular: { type: Number },
+    representanteApellidoPaterno: { type: String, required: true },
+    representanteApellidoMaterno: { type: String, required: true },
+    contactoNumeroFijo: { type: String, required: true },
+    contactoNumeroCelular: { type: String, required: true },
     contactoCorreo: { type: String, required: true },
-    FechaSolicitud: { type: Date, default: Date.now }
+    fechaSolicitud: { type: String, required: true },
+    IDe: { type: String, required: true },
+    IDa: { type: String, required: true },
+    userName: { type: String, required: true },
+    userPassword: { type: String, required: true },
+    userCuestion: { type: String, required: true },
+    userAnswer: { type: String, required: true },
+    datosFiscalesMEXFolioF: { type: String, required: true },
+    datosFiscalesMEXFolioCP: { type: String, required: true },
+    datosFiscalesMEXNombreEmpresa: { type: String, required: true },
+    datosFiscalesMEXColonia: { type: String, required: true },
+    datosFiscalesMEXCalle: { type: String, required: true },
+    datosFiscalesMEXNoExterior: { type: String, required: true },
+    datosFiscalesMEXNoInterior: { type: String, required: true },
+    datosFiscalesMEXEntreCalles: { type: String, required: true },
+    datosFiscalesMEXDireccionFiscal: { type: String, required: true },
+    datosFiscalesMEXRazonSocial: { type: String, required: true },
+    datosFiscalesMEXEstadoMexicano: { type: String, required: true },
+    datosFiscalesMEXCiudadMexicana: { type: String, required: true },
+    datosFiscalesMEXRFC: { type: String, required: true },
+    datosFiscalesMEXTelefono: { type: String, required: true },
+    datosFiscalesEUAFolioCP: { type: String, required: true },
+    datosFiscalesEUANombreEmpresa: { type: String, required: true },
+    datosFiscalesEUAColonia: { type: String, required: true },
+    datosFiscalesEUACalle: { type: String, required: true },
+    datosFiscalesEUANoExterior: { type: String, required: true },
+    datosFiscalesEUANoInterior: { type: String, required: true },
+    datosFiscalesEUAEntreCalles: { type: String, required: true },
+    datosFiscalesEUACodigoPostal: { type: String, required: true },
+    datosFiscalesEUATAXID: { type: String, required: true },
+    datosFiscalesEUATelefono: { type: String, required: true },
+    checkBoxDEMO1: { type: String, required: true },
+    checkBoxDEMO2: { type: String, required: true },
+    checkBoxDEMO3: { type: String, required: true },
+    checkBoxDEMO4: { type: String, required: true },
+    checkBoxDEMO5: { type: String, required: true },
+    checkBoxOTHERS1: { type: String, required: true },
+    checkBoxOTHERS2: { type: String, required: true },
+    checkBoxOTHERS3: { type: String, required: true },
+    checkBoxOTHERS4: { type: String, required: true },
+    checkBoxOTHERS5: { type: String, required: true },
+    checkBoxOTHERS6: { type: String, required: true },
+    checkBoxOTHERS7: { type: String, required: true },
+    checkBoxOTHERS8: { type: String, required: true },
+    checkBoxOTHERS9: { type: String, required: true },
+    checkBoxOTHERS10: { type: String, required: true },
+    checkBoxOTHERS11: { type: String, required: true },
+    checkBoxOTHERS12: { type: String, required: true },
+    checkBoxOTHERS13: { type: String, required: true },
+    checkBoxOTHERS14: { type: String, required: true },
+    checkBoxOTHERS15: { type: String, required: true },
+    checkBoxOTHERS16: { type: String, required: true }
 });
 
-ProfileClientSchema.methods.encryptPassword = async(password) => {
+clientes.methods.encryptPassword = async(userPassword) => {
     const salt = await bcrypt.genSalt(10);
-    const hash = bcrypt.hash(password, salt);
+    const hash = bcrypt.hash(userPassword, salt);
     return hash;
 };
 
-ProfileClientSchema.methods.matchPassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
+clientes.methods.matchPassword = async function(userPassword) {
+    return await bcrypt.compare(userPassword, this.userPassword);
 }
 
-module.exports = mongoose.model('perfilesexternos', ProfileClientSchema);
+module.exports = mongoose.model('Clientes', clientes);
